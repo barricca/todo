6cfb73852bb0244abbcc0aa7fbaca4ef711274f0
Williams B. Gomes <williamsgomes45@gmail.com>
2025-07-20 00:59:21 -0300

    Estruturando a base - Criando componente Icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
+import CheckIcon from "./assets/icons/check.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
+
+import { Icon } from "./components/icon";
 import { Text } from "./components/Text";
 
 export function App() {
 	return (
-		<div className="flex flex-col gap-2">
-			<Text variant="body-sm-bold" className="text-pink-base">
-				Olá mundo!
-			</Text>
-			<Text className="text-green-base">Olá mundo!</Text>
-			<Text variant="body-md-bold">Olá mundo!</Text>
-			<Text>Levar o dog pra passear</Text>
+		<div className="grid gap-3">
+			<div className="flex flex-col gap-2">
+				<Text variant="body-sm-bold" className="text-pink-base">
+					Olá mundo!
+				</Text>
+				<Text className="text-green-base">Olá mundo!</Text>
+				<Text variant="body-md-bold">Olá mundo!</Text>
+				<Text>Levar o dog pra passear</Text>
+			</div>
+
+			<div className="flex gap-1">
+				<Icon svg={TrashIcon} className="fill-green-base" />
+				<Icon svg={CheckIcon} />
+				<Icon svg={PlusIcon} />
+				<Icon svg={SpinnerIcon} animate className="fill-pink-dark" />
+				<Icon svg={PencilIcon} />
+				<Icon svg={XIcon} />
+			</div>
 		</div>
 	);
 }
