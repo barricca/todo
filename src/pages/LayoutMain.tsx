84ad1a4b451463e86cb5eefde3aa6042c6cce420
Williams B. Gomes <williamsgomes45@gmail.com>
2025-07-21 00:41:52 -0300

    Montando layout - Estruturando corpo do layout

--- src/pages/LayoutMain.tsx
+++ src/pages/LayoutMain.tsx
@@ -1,32 +1,18 @@
-import { NavLink, Outlet } from "react-router";
-import { Container } from "../components/Container";
-import { Text } from "../components/Text";
+import { Outlet } from "react-router";
+import { Footer } from "../core-components/footer";
+import { Header } from "../core-components/Header";
+import { MainContent } from "../core-components/MainContent";
 
 export function LayoutMain() {
 	return (
 		<>
-			<Container as="header" className="mt-3 md:mt-20">
-				Olá Mundo! HEADER
-			</Container>
+			<Header />
 
-			<main className="mt-4 md:mt-8">
+			<MainContent className="mt-4 md:mt-8">
 				<Outlet />
-			</main>
+			</MainContent>
 
-			<footer className="my-5 md:my-10">
-				<nav className="flex items-center justify-center gap-4">
-					<NavLink to="/">
-						<Text variant="body-sm-bold" className="text-gray-300">
-							Tarefas
-						</Text>
-					</NavLink>
-					<NavLink to="/components">
-						<Text variant="body-sm-bold" className="text-gray-300">
-							Componentes
-						</Text>
-					</NavLink>
-				</nav>
-			</footer>
+			<Footer />
 		</>
 	);
 }
