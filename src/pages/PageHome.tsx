763c8264904a02fcdab6603db40900d327818761
Williams B. Gomes <williamsgomes45@gmail.com>
2025-07-21 00:51:04 -0300

    Montando layout - Criando componente de resumo das tarefas

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,5 +1,12 @@
 import { Container } from "../components/Container";
+import { TaskSummary } from "../core-components/TaskSummary";
 
 export function PageHome() {
-	return <Container>Página Home!</Container>;
+	return (
+		<Container as="article" className="space-y-3">
+			<header className="flex items-center justify-between">
+				<TaskSummary />
+			</header>
+		</Container>
+	);
 }
